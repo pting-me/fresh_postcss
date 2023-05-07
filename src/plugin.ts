be15398395b57e8c5b9719b4df8b90fdb18896fc
Philip Ting <pting.me+gh@gmail.com>
2023-05-07 10:06:35 -0700

    added process function

--- src/plugin.ts
+++ src/plugin.ts
@@ -18,26 +18,16 @@ export interface Config {
   plugins?: AcceptedPlugin[];
 }
 
-export function freshPostcss(config: Config): Plugin {
-  const {
-    plugins,
-    from = "./static/style.css",
-    to,
-    ...processOptions
-  } = config;
-
+function createPlugin() {
   let cssText = "";
 
-  if (to) {
-    console.warn(
-      yellow("Warning (fresh_postcss):"),
-      'Using the "to" option will write a file using "Deno.writeFile".\n',
-      "It may work locally, but will not work with Deno Deploy.\n",
-      "https://deno.com/deploy/docs/runtime-fs\n",
-    );
-  }
-
-  const process = async () => {
+  const process = async (config: Config = {}) => {
+    const {
+      plugins,
+      from = "./static/style.css",
+      to,
+      ...processOptions
+    } = config;
     const css = await Deno.readTextFile(from);
 
     if (to) {
@@ -54,21 +44,46 @@ export function freshPostcss(config: Config): Plugin {
     }
   };
 
-  void process();
+  const freshPostcss = (config: Config = {}): Plugin => {
+    if (config.to) {
+      console.warn(
+        yellow("Warning (fresh_postcss):"),
+        'Using the "to" option will write a file using "Deno.writeFile".\n',
+        "It may work locally, but will not work with Deno Deploy.\n",
+        "https://deno.com/deploy/docs/runtime-fs\n",
+      );
+    }
 
-  return {
-    name: "postcss",
-    render: (ctx) => {
-      const res = ctx.render();
+    void process(config);
 
-      if (res.requiresHydration) {
-        void process();
-      }
+    return {
+      name: "postcss",
+      render: (ctx) => {
+        const res = ctx.render();
 
-      return {
-        styles: [{ cssText, id: STYLE_ELEMENT_ID }],
-        scripts: [],
-      };
-    },
+        if (res.requiresHydration) {
+          // This will usually require double refresh for changes to be updated
+          void process(config);
+        }
+
+        if (config.to) {
+          return {
+            styles: [],
+            scripts: [],
+          };
+        }
+
+        return {
+          styles: [{ cssText, id: STYLE_ELEMENT_ID }],
+          scripts: [],
+        };
+      },
+    };
   };
+
+  return { process, freshPostcss };
 }
+
+const { process, freshPostcss } = createPlugin();
+
+export { freshPostcss, process };
